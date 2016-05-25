3947beb8080f8464c0dfa7fbd6e69361b6170d78
shirogani <shirogani.mail@gmail.com>
2016-05-25 22:01:58 +0900

    item（記事）モデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  has_many :items
+
   before_save { self.email = email.downcase }
 
   validates :name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507072450) do
+ActiveRecord::Schema.define(version: 20160525125805) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.text     "content",    limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "items", ["user_id"], name: "index_items_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255
@@ -23,4 +33,5 @@ ActiveRecord::Schema.define(version: 20160507072450) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
+  add_foreign_key "items", "users"
 end
