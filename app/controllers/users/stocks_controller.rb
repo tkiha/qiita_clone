1687cf387cf7c257dbf998cd846e461e25efc6ec
shirogani <shirogani.mail@gmail.com>
2016-07-01 01:21:31 +0900

    fix

--- app/controllers/users/stocks_controller.rb
+++ app/controllers/users/stocks_controller.rb
@@ -3,14 +3,14 @@ class Users::StocksController < ApplicationController
   before_action :correct_user
 
   def index
-    @items = current_user.items_stocked.recent
+    @items = @user.items_stocked.recent
   end
 
   private
 
   def correct_user
-    user = User.find params[:user_id]
-    unless user == current_user
+    @user = User.find params[:user_id]
+    unless @user == current_user
       flash[:danger] = "権限がありません。"
       redirect_to root_url
     end
