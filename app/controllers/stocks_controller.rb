877bdd6b1c33527c84a1f8e06b93fe97c058c0cb
shirogani <shirogani.mail@gmail.com>
2016-07-02 21:53:58 +0900

    記事をストックをAjaxで行う処理をすっきりさせた

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,14 +3,11 @@ class StocksController < ApplicationController
   before_action :set_item
 
   def create
-    current_user.stocks.find_or_create_by!(item: @item)
-    render js: 'location.reload();'
+    @stock = current_user.stocks.find_or_create_by!(item: @item)
   end
 
   def destroy
-    stock = current_user.stocks.find_by!(item: @item)
-    stock.destroy
-    render js: 'location.reload();'
+    current_user.stocks.find_by!(item: @item).destroy
   end
 
   private
--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -2,12 +2,12 @@ module StocksHelper
   def item_stock_link_to(item, stock)
     if logged_in?
       if stock
-        link_to "ストックを解除", item_stock_path(item), remote: true, method: :delete, class: 'btn btn-danger btn-block'
+        link_to 'ストックを解除', item_stock_path(item), remote: true, method: :delete, class: 'btn btn-danger btn-block'
       else
-        link_to "ストック", item_stock_path(item), remote: true, method: :post, class: 'btn btn-success btn-block'
+        link_to 'ストック', item_stock_path(item), remote: true, method: :post, class: 'btn btn-success btn-block'
       end
     else
-      link_to "ストック", login_path, class: 'btn btn-success btn-block'
+      link_to 'ストック', login_path, class: 'btn btn-success btn-block'
     end
   end
 end
