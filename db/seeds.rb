6a0cd15c0c92bce35886fcfde66f2b5054d71d2d
shirogani <shirogani.mail@gmail.com>
2016-07-09 14:48:57 +0900

    簡単なseedsを準備

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+users = User.create([
+                        { name: 'テスター1', email: 'test@test.com', password: 'test' },
+                        { name: 'テスター2', email: 'test2@test.com', password: 'test' },
+                        { name: 'テスター3', email: 'test3@test.com', password: 'test' },
+                        { name: 'テスター4', email: 'test4@test.com', password: 'test' },
+                    ])
+
+tags = Tag.create([
+                      { name: 'タグ1' },
+                      { name: 'タグ2' },
+                      { name: 'タグ3' },
+                      { name: 'タグ4' },
+                  ])
+
+items = Item.create([
+                        { user: users.first, title: 'タイトル1', content: "あああああああああ\r\n\r\n- a\r\n- a\r\n- a\r\n\r\n> あああああああああああ\r\n\r\n# a\r\n\r\n## aa\r\n\r\n### aaa\r\n\r\n*aaaaaaa*\r\n\r\n**aaaaaaaa**\r\n\r\n`aaaaaaaaaaaaa`\r\n\r\n```\r\naaaaaaaaaaa\r\n````\r\n" },
+                        { user: users.first, title: 'タイトル2', content: "# 投稿2" },
+                        { user: users.first, title: 'タイトル3', content: "# 投稿3" },
+                        { user: users.second, title: 'タイトル4', content: "# 投稿4" },
+                        { user: users.second, title: 'タイトル5', content: "# 投稿5" },
+                        { user: users.second, title: 'タイトル6', content: "# 投稿6" },
+                    ])
+
+TagItem.create([
+                   { item: items.first, tag: tags.first },
+                   { item: items.first, tag: tags.second },
+                   { item: items.second, tag: tags.first },
+                   { item: items.second, tag: tags.second },
+                   { item: items.second, tag: tags.third },
+                   { item: items.third, tag: tags.fourth },
+               ])
+
