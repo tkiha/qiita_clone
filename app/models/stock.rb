c4ce8765b4bca3d925375f5a8248f9848295f38a
shirogani <shirogani.mail@gmail.com>
2016-07-11 00:54:03 +0900

    自分の投稿をストックできないようにした。

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,4 +5,12 @@ class Stock < ActiveRecord::Base
   validates :user_id, presence: true
   validates :item_id, presence: true
   validates :user_id, uniqueness: { scope: :item_id }
+  validate :user_cannot_stock_self_posted_item
+
+  def user_cannot_stock_self_posted_item
+    item = Item.find item_id
+    if user_id == item.user.id
+      errors.add(:follower_id, ': 自分の投稿はストックできません。')
+    end
+  end
 end
