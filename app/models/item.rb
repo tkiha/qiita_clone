1e9cb27417d896ccd9318f9332881fad26f1e04e
shirogani <shirogani.mail@gmail.com>
2016-07-11 00:00:32 +0900

    コメントつきの記事を削除できるように

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :stocks, dependent: :destroy
   has_many :users_stocking, through: :stocks, source: :user
   has_many :tag_items, dependent: :destroy
